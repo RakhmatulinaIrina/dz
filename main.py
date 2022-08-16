58ac3c271302efb04502dc20da3a7653d0c455fc
RakhmatulinaIrina <110806253+RakhmatulinaIrina@users.noreply.github.com>
2022-08-16 12:05:28 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+from utils import load_candidates, get_all, get_by_pk, get_by_skill
+
+FILENAME = 'candidates.json'
+data = get_all(load_candidates(FILENAME))
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    str = '<pre>'
+    for i in data:
+        str += f'{i} \n \n'
+    str += '</pre>'
+    return str
+
+@app.route('/candidates/<int:pk>')
+def get_user(pk):
+    user = get_by_pk(pk, data)
+    if user:
+        str = f'<img src = "{user.picture}">'
+        str += f'<pre> {user} </pre>'
+    else:
+        str = 'not found'
+    return str
+
+@app.route('/skills/<x>')
+def get_users(x):
+    users = get_by_skill(x)
+    if users:
+        str = '<pre>'
+        for i in users:
+            str += f'{i} \n \n'
+        str += '</pre>'
+    else:
+        str = 'not found'
+    return str
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
